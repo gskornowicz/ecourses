51e79c6219b8b6decf56ca2b547c6fe97ad275e6
Grzegorz Skornowicz <g.skornowicz@gmail.com>
2016-08-15 19:08:59 +0200

    added captcha secret and public key variables

--- config.php
+++ config.php
@@ -6,6 +6,9 @@ $db_name = "elearning";
 $db_user = "elearning";
 $db_password = "ThJfay5UAGMtyZmG";
 
+// recaptcha settings
+$secret_key = "6LeB1SYTAAAAAIGA5HR2SaHZa-rcCDTnVF4N9Wfc"; // need to be changed when hosted outside of "localhost" dev environment
+$public_key = "6LeB1SYTAAAAAE8UZ7GU_YujTkJEUZ-bWogF46xI"; // need to be changed when hosted outside of "localhost" dev environment
 
 // other settings
-$debug_mode = 0; // enables include debug.php on platform
+$debug_mode = 1; // enables include debug.php on platform
