3d4a2e5b51c29fd040aee97e289d2b59f2f3eb8a
Grzegorz Skornowicz <g.skornowicz@gmail.com>
2016-08-17 18:24:35 +0200

    added error handling for database operations

--- functions/register.php
+++ functions/register.php
@@ -9,98 +9,132 @@ if(@isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true)
 }
 
 
+// assigning post variables
+$login = $_POST['login'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$retype_password = $_POST['retype_password'];
+$rules = $_POST['rules'];
+
 //recaptcha 2.0
-$captcha_check = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret_key.'&response='.$_POST['g-recaptcha-response']); // $secret key is in config.php
-$answer = json_decode($captcha_check);
-
-// if captcha is success then start registration process
-if($answer->success == true)
+// if enabled do:
+if($recaptcha_enabled == true)
 {
-    $login = $_POST['login'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $retype_password = $_POST['retype_password'];
-    $rules = $_POST['rules'];
+    $captcha_check = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret_key.'&response='.$_POST['g-recaptcha-response']); // $secret key is in config.php
+    $answer = json_decode($captcha_check);
 
-
-    // POST data check
-    //// check login for length and special characters
-    if(strlen($login)<3 || strlen($login)>23 || !ctype_alnum($login))
+    // setting up failed flag if recaptcha failed
+    if($answer->success == false)
     {
-        $_SESSION['bad_login'] = true;
+        $_SESSION['recaptcha_failed'] = true;
     }
+}
 
-    // check email with regular expression (if it has xxx@domain.pl)
-    if(preg_match("/^[a-z0-9](\.?[a-z0-9_-]){0,}@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/", $email) === false)
-    {
-        $_SESSION['bad_email'] = true;
-    }
+// POST data check
+//// check login for length and special characters
+if(strlen($login)<3 || strlen($login)>23 || !ctype_alnum($login))
+{
+    $_SESSION['bad_login'] = true;
+}
 
-    // check password for at least 6 characters long and lesss than 64
-    if(strlen($password)<6 || strlen($password)>64)
-    {
-        $_SESSION['bad_password'] = true;
-    }
+// check email with regular expression (if it has xxx@domain.pl)
+if(preg_match("/^[a-z0-9](\.?[a-z0-9_-]){0,}@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/", $email) === false)
+{
+    $_SESSION['bad_email'] = true;
+}
 
-    // check retype of password
-    if($password != $retype_password)
-    {
-        $_SESSION['bad_password_retype'] = true;
-    }
+// check password for at least 6 characters long and lesss than 64
+if(strlen($password)<6 || strlen($password)>64)
+{
+    $_SESSION['bad_password'] = true;
+}
+
+// check retype of password
+if($password != $retype_password)
+{
+    $_SESSION['bad_password_retype'] = true;
+}
 
 
+// connection to DB and checking for duplication in login or email
+try
+{
     $db_connection = new mysqli($db_host, $db_user, $db_password, $db_name);
 
-    // check if login or email are not already in database
-    if(mysqli_num_rows($result = $db_connection->query(sprintf("SELECT * FROM users WHERE login='%s'",mysqli_real_escape_string($db_connection,$login))))>0)
+    // if connection had error, throw exception
+    if($db_connection->connect_errno!=0)
     {
-        $_SESSION['login_already_in_database'] = true;
-    }
-    if(mysqli_num_rows($result = $db_connection->query(sprintf("SELECT * FROM users WHERE email='%s'",
-             mysqli_real_escape_string($db_connection,$email))))>0)
-    {
-        $_SESSION['email_already_in_database'] = true;
-    }
-
-    // if anything was wrong with registration, go back to registration!
-    if(isset($_SESSION['bad_password_retype']) ||
-                isset($_SESSION['bad_login']) ||
-                isset($_SESSION['bad_email']) ||
-                isset($_SESSION['bad_password']) ||
-                isset($_SESSION['login_already_in_database']) ||
-                isset($_SESSION['email_already_in_database']))
-    {
-        header('Location: ../registration.php');
+        throw new Exception("EXCEPTION: Error number: ".mysqli_connect_errno()." Error message: ".mysqli_connect_error());
+        exit("Database error, please report this to admin and try again later");
     }
     else
+    {
+        // check if login or email are not already in database
+        if(mysqli_num_rows($result = $db_connection->query(sprintf("SELECT id FROM users WHERE login='%s'",
+                                                                   mysqli_real_escape_string($db_connection,$login))))>0)
+        {
+            $_SESSION['login_already_in_database'] = true;
+        }
+        if(mysqli_num_rows($result = $db_connection->query(sprintf("SELECT id FROM users WHERE email='%s'",
+                                                                    mysqli_real_escape_string($db_connection,$email))))>0)
+        {
+            $_SESSION['email_already_in_database'] = true;
+        }
+    }
+
+}
+catch(Exception $error)
+{
+    error_log($error->getMessage(), 0);
+}
+
+
+
+
+// if anything was wrong with registration, go back to registration form!
+if(isset($_SESSION['bad_password_retype']) ||
+            isset($_SESSION['recaptcha_failed']) ||
+            isset($_SESSION['bad_login']) ||
+            isset($_SESSION['bad_email']) ||
+            isset($_SESSION['bad_password']) ||
+            isset($_SESSION['login_already_in_database']) ||
+            isset($_SESSION['email_already_in_database']))
+{
+    header('Location: ../registration.php');
+}
+else
+{
+    //if no earlier error has been encountered, insert post form data into database
+    try
     {
         //default hash password before inserting into database
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
         //database sanitised insert
-       if($db_connection->query(sprintf("INSERT INTO users (`id`, `login`, `password`, `email`) VALUES (NULL, '%s', '%s', '%s')",
+        if($db_connection->query(sprintf("INSERT INTO users (`id`, `login`, `password`, `email`) VALUES (NULL, '%s', '%s', '%s')",
                                     mysqli_real_escape_string($db_connection,$login),
                                     mysqli_real_escape_string($db_connection,$password),
                                     mysqli_real_escape_string($db_connection,$email))))
-       {
-           $_SESSION['account_created'] = true;
+        {
+            //on success, flag account created and go to login form
+            $_SESSION['account_created'] = true;
             header('Location: ../index.php');
-       }
+        }
         else
         {
-            echo "db error";
+            // if not succeded, throw exception
+            throw new Exception("EXCEPTION: Error number: ".mysqli_connect_errno()." Error message: ".mysqli_connect_error());
+            echo "DB connection error see logs for error";
         }
 
+
+    }
+    catch(Exception $error)
+    {
+        error_log($error->getMessage(), 0);
+        exit("Database error, please report this to admin and try again later");
     }
 
-    mysqli_close($db_connection);
-}
-else
-{
-    $_SESSION['recaptcha_failed'] = true;
-   header('Location: ../registration.php');
 }
 
-
-
-?>
+mysqli_close($db_connection);
